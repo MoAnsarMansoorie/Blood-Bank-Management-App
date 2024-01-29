cacf8286973e966cd9ba06120c59b7524c068d16
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-01-30 00:40:41 +0530

    login action and reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import {Routes, Route} from "react-router-dom"
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,14 @@
+import { userLogin } from "../redux/features/auth/authAction";
+import store from "../redux/store";
+
 export const loginHandler = (e, email, password, role) => {
   e.preventDefault();
   try {
     console.log("login", e, email, password, role);
+    if (!role || !email || !password) {
+      return alert("Please Privde All Feilds");
+    }
+    store.dispatch(userLogin({email, password, role}))
   } catch (error) {
     console.log(error);
   }
